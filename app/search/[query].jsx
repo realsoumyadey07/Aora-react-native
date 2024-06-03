90cca3282c8b4115861bbf1e3601c42fd4a4bb56
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-06-03 22:20:14 +0530

    search feature is implemented

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,13 +1,57 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Alert, FlatList, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import SearchInput from "../../components/SearchInput";
+import EmptyState from "../../components/EmptyState";
+import useAppWrite from "../../lib/useAppwrite";
+import { searchPosts } from "../../lib/appwrite";
+import VideoCard from "../../components/VideoCard";
+import { useLocalSearchParams } from "expo-router";
 
 const Search = () => {
+  const { query }= useLocalSearchParams();
+  const { data: posts, refetch } = useAppWrite(()=>searchPosts(query));
+
+  useEffect(()=> {
+    refetch();
+  },[query])
+  
   return (
-    <View>
-      <Text>Query</Text>
-    </View>
-  )
-}
+    <>
+      <SafeAreaView className="bg-primary h-full">
+        <FlatList
+          data={posts}
+          // data={[]}
+          keyExtractor={(item) => item.$id}
+          renderItem={({ item }) => (
+            <VideoCard video={item}/>
+          )}
+          ListHeaderComponent={() => (
+            <View className="my-6 px-4">
+                  <Text className="font-pmedium text-sm text-gray-100">
+                    Search result
+                  </Text>
+                  <Text className="text-2xl font-psemibold text-white">
+                    {query}
+                  </Text>
+                  <View className="mt-6 mb-8">
+                    <SearchInput initialQuery={query} />
+                  </View>
+            </View>
+          )}
+          ListEmptyComponent={() => (
+            <EmptyState
+              title="No Videos Found"
+              subTitle="No videos found for this search query"
+            />
+          )}
+          
+        />
+      </SafeAreaView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
+};
 
-export default Search
-
+export default Search;
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,27 +1,32 @@
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, Image, TextInput, TouchableOpacity, View } from "react-native";
 import React, { useState } from "react";
 import { icons } from "../constants";
+import { router, usePathname } from "expo-router";
 
-const SearchInput = ({
-  title,
-  value,
-  placeHolder,
-  handleChangeText,
-  otherStyles,
-  ...props
-}) => {
-    const [showPassword, setShowPassword] = useState(false)
+const SearchInput = ({ initialQuery }) => {
+    const pathName = usePathname();
+    const [query, setQuery] = useState(initialQuery || "");
   return (
       <View className="border-2 border-black-200 w-full h-14 px-4 bg-black-100 rounded-2xl focus:border-secondary-100 flex-row items-center space-x-4">
         <TextInput 
             className="text-base mt-0.5 text-white flex-1 font-pregular" 
-            value={value}
+            value={query}
             placeholder="Search for a video topic"
-            placeholderTextColor="#d7d7d7"
-            onChangeText={handleChangeText}
-            secureTextEntry={title==="Password" && !showPassword}
+            placeholderTextColor="#CDCDE0"
+            onChangeText={(e)=> setQuery(e)}
         />
-        <TouchableOpacity>
+        <TouchableOpacity
+          onPress={()=> {
+            if(!query){
+              return (
+                Alert.alert("Missing query", "Please input something to search here")
+              )
+            }
+
+            if(pathName.startsWith('/search')) {router.setParams({ query })}
+            else {router.push(`/search/${query}`)}
+          }}
+        >
             <Image
                 source={icons.search}
                 className="w-5 h-5"
